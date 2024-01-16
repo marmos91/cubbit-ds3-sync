58fbd39df287754484b855683e89a208c1b0de69
Marco Moschettini <m.marmos@gmail.com>
2024-01-16 21:02:56 +0100

    chore: scaffold project

---
+++ CubbitDS3Sync/CubbitDS3SyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  CubbitDS3SyncApp.swift
+//  CubbitDS3Sync
+//
+//  Created by Marco Moschettini on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CubbitDS3SyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
