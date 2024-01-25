7569bf5e4f5568b08f0441c754bc54861a78e6a6
Marco Moschettini <m.marmos@gmail.com>
2024-01-25 11:23:13 +0100

    feat: add withRetries utility

---
+++ DS3Lib/Utils/ControlFlow.swift
@@ -0,0 +1,37 @@
+import Foundation
+import os.log
+
+enum ControlFlowError: Error {
+    case maxRetriesReached
+}
+
+/// Retries a block of code a given number of times before throwing an error
+/// - Parameters:
+///   - retries: the number of retries
+///   - logger: optional logger
+///   - block: the block of code to retry
+/// - Throws: the error thrown by the block of code
+/// - Returns: the result of the block of code
+func withRetries<T>(
+    retries: Int,
+    withLogger logger: Logger? = nil,
+    block: @escaping() async throws -> T
+) async throws -> T {
+    var retries = retries
+    
+    while retries > 0 {
+        do {
+            return try await block()
+        } catch {
+            retries -= 1
+            
+            logger?.error("An error occurred: \(error): remaining retries \(retries)")
+            
+            if retries == 0 {
+                throw error
+            }
+        }
+    }
+    
+    throw ControlFlowError.maxRetriesReached
+}
